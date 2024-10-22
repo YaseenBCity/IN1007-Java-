21315e7a604bf6fd890c44a385b262d215907c59
YaseenBCity <Yaseen.Barlas@city.ac.uk>
2024-10-22 13:17:55 +0100

    Create Exercise7.java

---
+++ Week 4/src/Exercise7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Exercise7 {
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        int n, t;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many numbers you want: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter a new number:");
+            t = sc.nextInt();
+            list.add(t);
+        }
+        System.out.println("This is the reverse of the list:");
+        for (int j=list.size()-1; j>=0; j--){
+            if (j==0){
+                System.out.print(list.get(j));
+
+            }else{
+                System.out.print(list.get(j) + ",");
+            }
+        }
+
+    }
+}
