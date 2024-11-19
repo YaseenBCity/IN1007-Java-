44e182cb7ebb6e0dc77ded4fa4998d3797b73af4
YaseenBCity <Yaseen.Barlas@city.ac.uk>
2024-11-19 13:43:58 +0000

    Update Exercise2.java

--- Week 8/src/Exercise2.java
+++ Week 8/src/Exercise2.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Exercise2 {
     public static void main(String[] args) {
         Shop myShop = new Shop();
-        int[] myArray = {1,2,3};
+        int[] myArray = {1,2,3,4,5,6};
         Shop myShop2 = new Shop("bunugsstore",3,myArray);
 
         myShop.getName();
@@ -15,7 +15,7 @@ public class Exercise2 {
         boolean result = myMethod(myShop2);
         System.out.println(result);
 
-        int[] myArray2 = {2,1};
+        int[] myArray2 = {2,1,6};
         Buyer myBuyer = new Buyer(myShop2,myArray2);
 
         myMethod2(myBuyer);
